ab3f0cad9010fdbae226b82ffdf4ea8c49b1da5b
“nikhilyadav” <nikhil.yadav@obcc.de>
2020-09-15 15:01:03 +0200

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a simple Todo list applicaiton basec on React Framework.
+                This was built with the help from Traversy Media.
+                The tutorial can be found <a href="https://www.youtube.com/watch?v=sBws8MSXN7A">Here</a>
+            </p>
+        </React.Fragment>
+    )
+}
+
+export default About;
